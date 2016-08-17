832ce233bfc41145288f5b21092058e9f7d96b03
Harry Venables <venablesh@yahoo.co.uk>
2016-08-17 12:58:04 +0100

    Added inspect to the tower struct module

--- tower.ex
+++ tower.ex
@@ -7,3 +7,12 @@ defimpl Valid, for: Tower do
     h >= 0 && p != nil
   end
 end
+
+defimpl Inspect, for: Tower do
+  import Inspect.Algebra
+  def inspect(item, _options) do
+    metres = concat(to_string(item.height), "m:")
+    msg = concat([metres, break, item.name, ",", break, item.location, ",",
+      break, to_string(item.planemo)])
+  end
+end
