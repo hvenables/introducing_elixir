34bfbb99f5b948c1d931771e959b0b9218b6afbe
Harry Venables <venablesh@yahoo.co.uk>
2016-08-26 13:33:05 +0100

    Added restarting of failed processes and also started playing with try rescue blocks

--- mph_drop.ex
+++ mph_drop.ex
@@ -10,8 +10,9 @@ defmodule MphDrop do
       {planemo, distance} ->
         send(drop_pid, {self(), planemo, distance})
         convert(drop_pid)
-      {:EXIT, pid, reason} ->
-        IO.puts("Failure: #{inspect(pid)} #{inspect(reason)}")
+      {:EXIT, _pid, _reason} ->
+        new_drop_pid = spawn_link(ProcessDrop, :drop, [])
+        convert(new_drop_pid)
       {planemo, distance, velocity} ->
         mph_velocity = 2.23693629 * velocity
         IO.write("On #{planemo}, a fall of #{distance} meters ")
