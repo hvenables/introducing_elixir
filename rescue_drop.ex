8c1176cc8982e0ba4646d4aa53c18f4b900a7a4c
Harry Venables <venablesh@yahoo.co.uk>
2016-08-30 13:39:41 +0100

    Added specific rescue clauses

--- rescue_drop.ex
+++ rescue_drop.ex
@@ -1,15 +1,16 @@
 defmodule RescueDrop do
   def fall_velocity(planemo, distance) do
-    gravity = case planemo do
-      :earth -> 9.8
-      :moon -> 1.6
-      :mars -> 3.71
-    end
-
     try do
+      gravity = case planemo do
+        :earth -> 9.8
+        :moon -> 1.6
+        :mars -> 3.71
+      end
+
       :math.sqrt(2 * gravity * distance)
     rescue
-      _error -> _error
+      ArithmeticError -> {:error, "Distance must be non-negative"}
+      CaseClauseError -> {:error, "Unknown planemo #{planemo}"}
     end
   end
 end
