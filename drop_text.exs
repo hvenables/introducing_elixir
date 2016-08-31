6ce29036fc4e115283253ef9d0d0192f4e4c12f9
Harry Venables <venablesh@yahoo.co.uk>
2016-08-31 12:48:29 +0100

    Added assert in delta test

--- drop_text.exs
+++ drop_text.exs
@@ -10,4 +10,10 @@ defmodule DropTest do
   test "Mars calculation correct" do
     assert Drop.fall_velocity(:mars, 10) == :math.sqrt(2 * 3.71 * 10)
   end
+
+  test "Earth calculation correct" do
+    calculated = Drop.fall_velocity(:earth, 1)
+    assert_in_delta calculated, 4.4, 0.05,
+    "Result of #{calculated} is within 0.05 of 4.4"
+  end
 end
